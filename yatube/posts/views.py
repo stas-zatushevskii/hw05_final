bddc7cf2f1c396403414222a8645f5e13f98898f
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-09-08 19:44:39 +0300

    a

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import get_list_or_404, render, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from .models import Comment, Post, User
